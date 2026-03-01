f400576f3296cc3dbf28f637739f3e94bb51f7c8
ERROR <72090728+git404x@users.noreply.github.com>
2026-03-01 18:14:46 +0530

    refactor: migrate to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,22 +1,46 @@
 {
   description = "ERROR nixos configuration";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix.url = "github:danth/stylix";
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    programs-db = {
+      url = "github:wamserma/flake-programs-sqlite";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    nix-craft.url = "path:./NixOS/packages/nix-craft";
+  };
+
   outputs = {
               self,
               nixpkgs,
               nixpkgs-stable,
+              flake-parts,
               home-manager,
+              stylix,
               hyprland,
               programs-db,
-              stylix,
               ...
             }@inputs:
   
   let
+    system = "x86_64-linux";
 
     # system
     systemConfig = {
-      system = "x86_64-linux";
+      inherit system;
       hostname = "nix";
       timezone = "Asia/Kolkata";
       locale = "en_US.UTF-8";
@@ -29,96 +53,50 @@
       name = "px";
     };
 
-    system = systemConfig.system;
-    lib = nixpkgs.lib;
-
     pkgs = import nixpkgs {
       inherit system;
-      config = {
-        allowUnfree = true;
-      };
+      config.allowUnfree = true;
     };
 
     pkgs-stable = import nixpkgs-stable {
       inherit system;
-      config = {
-        allowUnfree = true;
-      };
+      config.allowUnfree = true;
     };
+
+    lib = nixpkgs.lib;
+    globalArgs = { inherit inputs pkgs-stable systemConfig userConfig; };
 
   in
-  {
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-    nixosConfigurations =
-    let
-      systemModules = [
-        stylix.nixosModules.default
-        ./NixOS/system/configuration.nix
-      ];
-
-      specialArgs = {
-        inherit pkgs-stable;
-        inherit systemConfig;
-        inherit userConfig;
-        inherit inputs;
+      perSystem = { pkgs, ... }: {
+        formatter = pkgs.nixpkgs-fmt;
       };
 
-    in
-    {
-      ${systemConfig.hostname} = lib.nixosSystem {
-        inherit system;
-        specialArgs = specialArgs;
-        modules = systemModules ++ [
-          ./NixOS/system/hardware-configuration.nix
-        ];
+      flake = {
+        nixosConfigurations = {
+          ${systemConfig.hostname} = lib.nixosSystem {
+            inherit system;
+            specialArgs = globalArgs;
+            modules = [
+              stylix.nixosModules.default
+              ./NixOS/system/configuration.nix
+              ./NixOS/system/hardware-configuration.nix
+            ];
+          };
+        };
+
+        homeConfigurations = {
+          "${userConfig.username}" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = globalArgs;
+            modules = [
+              stylix.homeModules.default
+              ./NixOS/user/home.nix
+            ];
+          };
+        };
       };
     };
-
-    homeConfigurations =
-    let
-      userModules = [
-        ./NixOS/user/home.nix
-        stylix.homeModules.default
-      ];
-      extraSpecialArgs = {
-        inherit pkgs-stable;
-        inherit systemConfig;
-        inherit userConfig;
-        inherit inputs;
-      };
-    in
-    {
-      ${userConfig.username} = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = extraSpecialArgs;
-        modules = userModules;
-      };
-    };
-  };
-
-  inputs = {
-
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    programs-db = {
-      url = "github:wamserma/flake-programs-sqlite";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland.url = "github:hyprwm/Hyprland";
-
-    stylix.url = "github:danth/stylix";
-
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-
-    nix-craft.url = "path:./NixOS/packages/nix-craft";
-
-  };
-
 }
