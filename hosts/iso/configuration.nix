d42c33488f587f99d9a21f4c3008fa5dd2b95130
ERROR <72090728+git404x@users.noreply.github.com>
2026-08-06 14:55:16 +0530

    feat: ci setup
    - cache builds using cachix
    - minimal iso builder

--- hosts/iso/configuration.nix
+++ hosts/iso/configuration.nix
@@ -1,11 +1,33 @@
 { pkgs, hostname, ... }:
 {
   networking.hostName = hostname;
+  networking.networkmanager.enable = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   environment.systemPackages = with pkgs; [
     git
     neovim
+    helix
+    nano
     cryptsetup
     pciutils
     usbutils
+    curl
+    wget
+    disko
+    parted
+    mkpasswd
+    networkmanager
+
+    # tools
+    btrfs-progs
+    dosfstools
+    e2fsprogs
+    ntfs3g
+    arch-install-scripts # provides genfstab, arch-chroot
   ];
 }
--- modules/nixos/core/config.nix
+++ modules/nixos/core/config.nix
@@ -24,23 +24,20 @@ in
       auto-optimise-store = true;
 
       # cache
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://hyprland.cachix.org"
-      ];
-      trusted-substituters = [
-        "https://cache.nixos.org/"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
+      substituters = [ "https://cache.nixos.org/" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+
       extra-substituters = [
+        "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
+        "https://git404x-dotfiles.cachix.org"
       ];
       extra-trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "git404x-dotfiles.cachix.org-1:LRBnrDoxA1OGdwGHgr2ecJY16Kq3KhzGObQEZf4qTjY="
       ];
+
     };
 
     # garbage-collection
